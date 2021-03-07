f814236f9deca4389af1483f8ea0eda3fba01ee9
balinth0rvath <horvath0balint@gmail.com>
2021-03-07 09:16:54 +0100

    Create logging. Fix bugs

--- Core/Src/receiver.c
+++ Core/Src/receiver.c
@@ -10,37 +10,55 @@
 #include "task.h"
 #include "nrf24.h"
 #include "semphr.h"
+#include "stdio.h"
 
 static uint8_t payload[10] = {};
 static uint8_t payload_length = 10;
 
 extern SemaphoreHandle_t sem_nRF24;
+static char message[64] = "";
+extern UART_HandleTypeDef huart2;
 
 void receiver_task()
 {
+  vTaskDelay(100);
+  sem_nRF24 = xSemaphoreCreateBinary();
+
   while(sem_nRF24 == NULL)
   {
     vTaskDelay(100);
   }
 
-  xSemaphoreTake(sem_nRF24, portMAX_DELAY);
+  sprintf(message,"Receiver: initializing... \n\r");
+  HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
 
   nRF24_SetDevice2();
 
-  nRF24_SetRFChannel(40);
-  nRF24_SetDataRate(nRF24_DR_2Mbps);
+  nRF24_CE_L();
+  nRF24_Init();
+
+  //nRF24_DisableAA(0xFF);
+
+  nRF24_SetRFChannel(42);
+  nRF24_SetDataRate(nRF24_DR_1Mbps);
   nRF24_SetCRCScheme(nRF24_CRC_2byte);
   nRF24_SetAddrWidth(3);
   static const uint8_t addr[] = {0x01, 0x10, 0xE3};
   nRF24_SetAddr(nRF24_PIPE1, addr);
   nRF24_SetRXPipe(nRF24_PIPE1, nRF24_AA_ON, 10);
+  nRF24_SetRXPipe(nRF24_PIPE1, nRF24_AA_OFF, 10);
   nRF24_SetTXPower(nRF24_TXPWR_0dBm);
   nRF24_SetOperationalMode(nRF24_MODE_RX);
   nRF24_ClearIRQFlags();
   nRF24_SetPowerMode(nRF24_PWR_UP);
   nRF24_CE_H();
 
+  sprintf(message,"Receiver: initialized \n\r");
+  HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+
   xSemaphoreGive(sem_nRF24);
+
+  vTaskDelay(100);
 
   nRF24_RXResult pipe;
 
@@ -54,9 +72,12 @@ void receiver_task()
     {
       pipe = nRF24_ReadPayload(payload, &payload_length);
       nRF24_ClearIRQFlags();
+      sprintf(message,"Receiver: packet arrived... \n\r");
+      HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
     }
+
     xSemaphoreGive(sem_nRF24);
 
-    vTaskDelay(450);
+    vTaskDelay(500);
   }
 }
--- Core/Src/transmitter.c
+++ Core/Src/transmitter.c
@@ -10,8 +10,11 @@
 #include "task.h"
 #include "semphr.h"
 #include "nrf24.h"
+#include "stdio.h"
 
 SemaphoreHandle_t sem_nRF24 = NULL;
+static char message[64] = "";
+extern UART_HandleTypeDef huart2;
 
 typedef enum {
   nRF24_TX_ERROR = 0,
@@ -24,15 +27,25 @@ static nRF24_TX_Result nRF24_TransmitPacket(uint8_t* payload, uint8_t payload_le
 
 void transmitter_task()
 {
-  sem_nRF24 = xSemaphoreCreateBinary();
+  vTaskDelay(200);
 
   if (sem_nRF24 == NULL)
     while (1);
 
+  xSemaphoreTake(sem_nRF24, portMAX_DELAY);
+
+  sprintf(message,"Transmitter: initializing... \n\r");
+  HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+
   nRF24_SetDevice1();
 
-  nRF24_SetRFChannel(40);
-  nRF24_SetDataRate(nRF24_DR_2Mbps);
+  nRF24_CE_L();
+  nRF24_Init();
+
+  //nRF24_DisableAA(0xFF);
+
+  nRF24_SetRFChannel(42);
+  nRF24_SetDataRate(nRF24_DR_1Mbps);
   nRF24_SetCRCScheme(nRF24_CRC_2byte);
   nRF24_SetAddrWidth(3);
   const uint8_t addr[] = {0x01, 0x10, 0xE3};
@@ -44,9 +57,13 @@ void transmitter_task()
   nRF24_SetOperationalMode(nRF24_MODE_TX);
   nRF24_ClearIRQFlags();
   nRF24_SetPowerMode(nRF24_PWR_UP);
-  nRF24_SetOperationalMode(nRF24_MODE_TX);
+
+  sprintf(message,"Transmitter: initialized \n\r");
+  HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
 
   xSemaphoreGive(sem_nRF24);
+
+  vTaskDelay(100);
 
   uint8_t payload[10] = {1,2,3,4,5,6,7,8,9,10};
   uint8_t payload_length = 10;
@@ -61,13 +78,42 @@ void transmitter_task()
 
     nRF24_SetDevice1();
 
+    //sprintf(message,"Transmitter: sending packet... \n\r");
+    //HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+
     ret = nRF24_TransmitPacket(payload, payload_length);
     otx = nRF24_GetRetransmitCounters();
 
-    xSemaphoreGive(sem_nRF24);
-
     otx_plot = (otx & nRF24_MASK_PLOS_CNT) >> 4;
     otx_arc = otx & nRF24_MASK_ARC_CNT;
+
+    //sprintf(message,"Transmitter: packet loss: %d auto retransmit count: %d \n\r", otx_plot, otx_arc);
+    //HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+    memset(message, 0, 64);
+    switch(ret)
+    {
+    case nRF24_TX_ERROR:
+      sprintf(message,"Transmitter: transmit error ERROR \n\r");
+      HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+      break;
+    case nRF24_TX_SUCCESS:
+      sprintf(message,"Transmitter: transmit SUCCESS \n\r");
+      HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+      break;
+    case nRF24_TX_TIMEOUT:
+      sprintf(message,"Transmitter: transmit error TIMEOUT \n\r");
+      HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+      break;
+    case nRF24_TX_MAXRT:
+      //sprintf(message,"Transmitter: transmit error MAXRT \n\r");
+      //HAL_UART_Transmit(&huart2, (uint8_t*) message, 64, 100);
+      break;
+    default:
+      break;
+    }
+
+    xSemaphoreGive(sem_nRF24);
+
     vTaskDelay(500);
   }
 }
